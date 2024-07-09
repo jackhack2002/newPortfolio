3b9eeee355ea3f8128af67703b7f470e809b65f6
jackhack2002 <jacobjabakumar2002@gmail.com>
2024-07-09 19:17:20 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'profile', component:ProfileComponent},
+  {path:'skills', component:SkillsComponent},
+  {path:'education', component:EducationComponent},
+  {path:"projects", component:ProjectComponent},
+  {path:'contact', component:ContactComponent},
+  {path:'',redirectTo:'profile',pathMatch:'full'},
+  {path:'**',redirectTo:'profile',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProjectComponent } from './project/project.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ProfileComponent,
+    ProjectComponent,
+    SkillsComponent,
+    ContactComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
